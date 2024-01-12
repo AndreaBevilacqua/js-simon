8f5f1242f9bcabe04f8fa7c10c2d1bbe6b894604
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-01-12 15:49:11 +0100

    added timer

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ console.log('js ok');
 // ^ MILESTONE 2 - Dopo 30 secondi il timer scompare e attraverso 5 prompt chiedo all'utente quali numeri ha visto
 // ^ MILESTONE 3 - Dopo che sono state registrare le 5 risposte verifico quali numeri sono stati individuati
 
+// ! =================
+// ! RECUPERO ELEMENTi
+// ! =================
+
+const display = document.getElementById('display');
 
 // ? =================
 // ? FUNZIONI GENERALI
@@ -14,7 +19,7 @@ console.log('js ok');
     return Math.floor(Math.random() * (max - min + 1)) + min;
  }
 
- // FUnzione per generare 5 numeri casuali da 1 a 50
+ // Funzione per generare 5 numeri casuali da 1 a 50
  const getRandomNumbers = () => {
     let casualNumbers = [];
     for(let i = 0; i < 5; i++) {
@@ -23,7 +28,23 @@ console.log('js ok');
     return casualNumbers;
  }
 
- 
+ // ~ ==================
+ // ~ FUNZIONI TEMPORALI
+ // ~ ==================
+
+ let seconds = 30;
+ display.innerText = seconds;
+
+ const countDown = setInterval(() => {
+    display.innerText = --seconds;
+    if(seconds === 0) {
+        display.innerText = '';
+        alert('TEMPO SCADUTO');
+        clearInterval(countDown);
+    }
+ }, 1000)
+
+
 
 
 
