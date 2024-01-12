430f92c1bafd795f04f8d9a18085839603f33132
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-01-12 15:21:56 +0100

    added milestones

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log('js ok');
+
+// ^ MILESTONE 1 - Creo 5 numeri casuali da 1 a 50 e li inserisco in pagina, una volta generati faccio partire un timer di 30 sec.
+// ^ MILESTONE 2 - Dopo 30 secondi il timer scompare e attraverso 5 prompt chiedo all'utente quali numeri ha visto
+// ^ MILESTONE 3 - Dopo che sono state registrare le 5 risposte verifico quali numeri sono stati individuati
